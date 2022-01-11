1d4fa825707bad825b73aa76e196d4eede28aca4
Marco Hung <37731008+marcohung818@users.noreply.github.com>
2022-01-11 04:55:17 +0000

    first update

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-testing
+import os
+import telebot
+
+API_KEY = os.getenv('API_KEY')
+bot = telebot.TeleBot(API_KEY)
+
+@bot.message_handler(command=['Greet'])
+def greet(message):
+  bot.reply_to(message, "Hey! Hows it going?")
